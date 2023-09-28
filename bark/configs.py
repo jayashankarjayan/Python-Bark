b456ab79b58d4408c00159bfb172dda502ec3a9e
jayashankar.jayan <jayashankar.jayan@gmail.com>
2023-09-29 00:34:49 +0530

    removed irrelevant validation

--- bark/configs.py
+++ bark/configs.py
@@ -5,17 +5,9 @@ class BarkConfig(BaseModel, validate_assignment=True):
     url: HttpUrl | IPvAnyAddress = "http://127.0.0.1"
     port: int = 8081
 
-    @field_validator("url")
-    def validate_url(cls, value: str):
-        if not value.startswith("http"):
-            raise TypeError("Invalid url provided")
-        return value.strip()
-
     @computed_field(repr=False)
     @property
     def bark_url(self) -> str:
-        if not self.url.startswith("http"):
-            raise TypeError("Invalid url provided")
         url = f"{self.url}:{self.port}"
         if url.endswith("/"):
             return url[:-1]
